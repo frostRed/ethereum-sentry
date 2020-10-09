c7083cdabd0236b57f43cf8559186db9d3c611a1
Artem Vorotnikov <artem@vorotnikov.me>
2020-10-10 01:58:17 +0300

    BlockBodies: fix transactions fetch

--- src/services/provider.rs
+++ src/services/provider.rs
@@ -11,6 +11,7 @@ use futures::{
 use rlp::{Decodable, DecoderError, Encodable, Rlp};
 use rlp_derive::{RlpDecodable, RlpEncodable};
 use std::fmt::Debug;
+use tracing::{span, Instrument, Level};
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum BlockId {
@@ -60,6 +61,26 @@ pub struct BlockBody {
     pub ommers: Vec<Header>,
 }
 
+fn web3_block_to_header<TX>(block: web3::types::Block<TX>) -> Option<Header> {
+    Some(Header {
+        parent_hash: block.parent_hash,
+        ommers_hash: block.uncles_hash,
+        beneficiary: block.author,
+        state_root: block.state_root,
+        transactions_root: block.transactions_root,
+        receipts_root: block.receipts_root,
+        logs_bloom: block.logs_bloom?,
+        difficulty: block.difficulty,
+        number: block.number?.as_u64().into(),
+        gas_limit: block.gas_limit,
+        gas_used: block.gas_used,
+        timestamp: block.timestamp.as_u64(),
+        extra_data: block.extra_data.0,
+        mix_hash: block.mix_hash?,
+        nonce: block.nonce?,
+    })
+}
+
 /// Provider of Ethereum blockchain data.
 #[async_trait]
 #[auto_impl(&, Box, Arc)]
@@ -104,26 +125,20 @@ impl Web3DataProvider {
 
         Ok(Self { client })
     }
-}
 
-fn web3_block_to_header<TX>(block: web3::types::Block<TX>) -> Option<Header> {
-    Some(Header {
-        parent_hash: block.parent_hash,
-        ommers_hash: block.uncles_hash,
-        beneficiary: block.author,
-        state_root: block.state_root,
-        transactions_root: block.transactions_root,
-        receipts_root: block.receipts_root,
-        logs_bloom: block.logs_bloom?,
-        difficulty: block.difficulty,
-        number: block.number?.as_u64().into(),
-        gas_limit: block.gas_limit,
-        gas_used: block.gas_used,
-        timestamp: block.timestamp.as_u64(),
-        extra_data: block.extra_data.0,
-        mix_hash: block.mix_hash?,
-        nonce: block.nonce?,
-    })
+    async fn get_transaction(&self, id: H256) -> anyhow::Result<Transaction> {
+        let raw = self
+            .client
+            .eth()
+            .transaction(id.into())
+            .await?
+            .ok_or_else(|| anyhow!("Transaction not found"))?
+            .raw
+            .ok_or_else(|| anyhow!("Raw transaction data absent"))?
+            .0;
+
+        Ok(rlp::decode(&*raw)?)
+    }
 }
 
 #[async_trait]
@@ -201,67 +216,80 @@ impl DataProvider for Web3DataProvider {
     fn get_block_bodies(&self, blocks: Vec<H256>) -> BoxStream<anyhow::Result<BlockBody>> {
         Box::pin(
             blocks
-                .into_iter()
-                .map(|id| async move {
-                    let block = self
-                        .client
-                        .eth()
-                        .block_with_txs(id.into())
-                        .await?
-                        .ok_or_else(|| anyhow!("Block not found"))?;
+                .iter()
+                .map(|&id| {
+                    async move {
+                        let block = self
+                            .client
+                            .eth()
+                            .block(id.into())
+                            .await?
+                            .ok_or_else(|| anyhow!("Block not found"))?;
 
-                    let header = web3_block_to_header(block.clone())
-                        .ok_or_else(|| anyhow!("Pending block?"))?;
+                        let header = web3_block_to_header(block.clone())
+                            .ok_or_else(|| anyhow!("Pending block?"))?;
 
-                    let ommers = block
-                        .uncles
-                        .iter()
-                        .map(|&uncle_hash| async move {
-                            web3_block_to_header(
-                                self.client
-                                    .eth()
-                                    .block(uncle_hash.into())
-                                    .await?
-                                    .ok_or_else(|| anyhow!("Uncle not found"))?,
-                            )
-                            .ok_or_else(|| anyhow!("Pending block?"))
+                        let (ommers, transactions) = futures::future::try_join(
+                            async {
+                                Ok::<_, anyhow::Error>(
+                                    block
+                                        .uncles
+                                        .iter()
+                                        .map(|&uncle_hash| async move {
+                                            web3_block_to_header(
+                                                self.client
+                                                    .eth()
+                                                    .block(uncle_hash.into())
+                                                    .await?
+                                                    .ok_or_else(|| anyhow!("Uncle not found"))?,
+                                            )
+                                            .ok_or_else(|| anyhow!("Pending block?"))
+                                        })
+                                        .collect::<FuturesUnordered<_>>()
+                                        .try_collect()
+                                        .await
+                                        .context("Failed to fetch uncles")?,
+                                )
+                            },
+                            async {
+                                Ok(block
+                                    .transactions
+                                    .iter()
+                                    .map(|&id| self.get_transaction(id))
+                                    .collect::<FuturesUnordered<_>>()
+                                    .try_collect()
+                                    .await
+                                    .context("Failed to fetch transactions")?)
+                            },
+                        )
+                        .await?;
+
+                        let assembled_block =
+                            ethereum::Block::new(header.into(), transactions, ommers);
+                        ensure!(
+                            id == assembled_block.header.hash(),
+                            "Hash mismatch: expected {}, found {}",
+                            id,
+                            assembled_block.header.hash()
+                        );
+
+                        let ethereum::Block {
+                            transactions,
+                            ommers,
+                            ..
+                        } = assembled_block;
+
+                        Ok(BlockBody {
+                            transactions,
+                            ommers,
                         })
-                        .collect::<FuturesUnordered<_>>()
-                        .try_collect()
-                        .await
-                        .context("Failed to fetch uncles")?;
-
-                    let transactions = block
-                        .transactions
-                        .iter()
-                        .map(|tx| {
-                            Ok(rlp::decode(
-                                &tx.raw
-                                    .as_ref()
-                                    .ok_or_else(|| anyhow!("Where's the raw tx data?"))?
-                                    .0,
-                            )?)
-                        })
-                        .collect::<anyhow::Result<_>>()?;
-
-                    let assembled_block = ethereum::Block::new(header.into(), transactions, ommers);
-                    ensure!(
-                        id == assembled_block.header.hash(),
-                        "Hash mismatch: expected {}, found {}",
-                        id,
-                        assembled_block.header.hash()
-                    );
-
-                    let ethereum::Block {
-                        transactions,
-                        ommers,
-                        ..
-                    } = assembled_block;
-
-                    Ok(BlockBody {
-                        transactions,
-                        ommers,
-                    })
+                    }
+                    .instrument(span!(
+                        Level::TRACE,
+                        "get block bodies",
+                        "blocks={:?}",
+                        blocks,
+                    ))
                 })
                 .collect::<FuturesUnordered<_>>(),
         )
